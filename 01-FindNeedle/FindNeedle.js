28dc43d4945a0f64d2568c274ad3fcf5259583d5
Jonathan Fernandez <arcancode@gmail.com>
2024-03-24 21:02:06 -0300

    dev1

--- 01-FindNeedle/FindNeedle.js
+++ 01-FindNeedle/FindNeedle.js
@@ -1,6 +1,12 @@
 function FindNeedle(haystack, needle) {
     // Your code here:
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === needle[0]) {
+            if (haystack.slice(i, i + needle.length) === needle) return i
+        }
 
+    }
+    return -1
 
 }
 
